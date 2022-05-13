ad27e09c6834f16011f95e723c01f4f824da7848
Suchan R <26240553+tncks@users.noreply.github.com>
2022-05-13 17:29:16 +0900

    ui
    ui

--- api/index.js
+++ api/index.js
@@ -1,16 +1,32 @@
 const app = require('express')();
 const { v4 } = require('uuid');
+const port = process.env.PORT || 3000;
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+app.get('/api/user', (req, res) => {
+  console.log("get api user")
+  // const path = `/api/`;
+  // const path = `/api/item/${v4()}`;
+  // res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  // res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.post('/api/user', (req, res) => {
+  console.log("post api user")
+  // const path = `/api/`;
+  // const path = `/api/item/${v4()}`;
+  // res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  // res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
+
+app.listen(port, () => {
+  console.log(`rest api server app listening on`)
+})
+
+//app.get('/api/item/:slug', (req, res) => {
+  //const { slug } = req.params;
+  //res.end(`Item: ${slug}`);
+//});
 
 module.exports = app;
